448c5c66063d11b4adc57c8f15133d82d70eb882
harsh dubey <hd84339@gmail.com>
2026-05-03 18:02:59 +0530

    converted shopkeeper app to Vite + fixed build issue

---
+++ shopkeeper-partner/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Shopkeeper Partner</h1>
+      <p>Welcome to your new React project.</p>
+    </div>
+  )
+}
+
+export default App
